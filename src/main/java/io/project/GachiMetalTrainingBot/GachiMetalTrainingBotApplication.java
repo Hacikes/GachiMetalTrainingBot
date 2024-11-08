7bcc5af2bb6a53a6d095c3ac496c9f3acdf234c4
Ruslan <volna4753@gmail.com>
2024-11-08 18:32:20 +0300

    Start project

---
+++ src/main/java/io/project/GachiMetalTrainingBot/GachiMetalTrainingBotApplication.java
@@ -0,0 +1,13 @@
+package io.project.GachiMetalTrainingBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachiMetalTrainingBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachiMetalTrainingBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/project/GachiMetalTrainingBot/GachiMetalTrainingBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.project.GachiMetalTrainingBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachiMetalTrainingBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
